2895f7489dda2d61fdadee7c752738b341b10806
AhmadDarKhalil <ahmad.darkhali@gmail.com>
2022-08-03 17:36:39 +0100

    add a function to generate video and support the overlay

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 """
 Created on Wed Aug  3 15:04:58 2022
 
-@author: ru20956
+@author: Ahmad Darkhalil
 """
 from vis import *
+import os
+
 json_files_path = '/home/ru20956/Downloads/visor_release_v3_train_val_test/sample_json'
 output_directory ='/home/ru20956/Downloads/out_pngs/sample_masks'
 input_resolution = (1920,1080)
 output_resolution= (1920,1080)
+is_overlay=True
+images_root_directory = '/home/ru20956/Downloads/DATA/Images_fixed_train_val_test'
 
 
-folder_of_jsons_to_masks_new(json_files_path, output_directory,input_resolution,output_resolution)
+folder_of_jsons_to_masks_new(json_files_path, output_directory,is_overlay=is_overlay,images_root_directory=images_root_directory,input_resolution=input_resolution,output_resolution=output_resolution)
+generate_video_from_images(os.path.join(output_directory+'/P28_103'), 3)
--- vis.py
+++ vis.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import torch
 
 
-def generate_masks_for_image(image_name, image_path, masks_info, output_directory,object_keys=None, input_resolution=(1920,1080), output_resolution=(1920,1080)):
+def generate_masks_for_image(image_name, image_path, masks_info, output_directory,object_keys=None,is_overlay=False, images_root_directory= '.', input_resolution=(1920,1080), output_resolution=(1920,1080)):
     import PIL
     from PIL import Image
     import numpy as np
@@ -57,19 +57,24 @@ def generate_masks_for_image(image_name, image_path, masks_info, output_director
         else:
             out_image = image_data
             
-        imwrite_indexed_2(output_directory + image_name, out_image)
+        imwrite_indexed_2(os.path.join(output_directory,image_name), out_image)
         
-        '''
-        image1 = image_name.replace("png", "jpg")
-        image1_val = Image.open('/jmain02/home/J2AD001/wwp01/axd53-wwp01/codes/interpolations/Images_fixed/'+os.path.basename(image1))
-        davis_palette = np.repeat(np.expand_dims(np.arange(0, 256), 1), 3, 1).astype(np.uint8)
-        davis_palette[:104, :] = [[0,0,0],[200, 0, 0], [0, 200, 0],[200, 128, 0], [0, 0, 200], [200, 0, 200], [0, 200, 200], [200, 200, 200],[252,93,82], [160,121,99], [164,188,119], [0,60,29], [75,237,255], [148,169,183], [96,74,207], [255,186,255], [255,218,231], [136,30,23], [231,181,131], [219,226,216], [0,196,107], [0,107,119], [0,125,227], [153,134,227], [91,0,56], [86,0,7], [246,207,195], [87,51,0], [125,131,122], [187,237,218], [46,57,59], [164,191,255], [37,29,57], [144,53,104], [79,53,54], [255,163,128], [255,233,180], [68,100,62], [0,231,199], [0,170,233], [0,20,103], [195,181,219], [148,122,135], [200,128,129], [46,20,10], [86,78,24], [180,255,188], [0,36,33], [0,101,139], [50,60,111], [188,81,205], [168,9,70], [167,91,59], [35,32,0], [0,124,28], [0,156,145], [0,36,57], [0,0,152], [89,12,97], [249,145,183],[255,153,170], [255,153,229], [184,143,204], [208,204,255], [11,0,128], [69,149,230], [82,204,194], [77,255,136], [6,26,0], [92,102,41], [102,85,61], [76,45,0], [229,69,69], [127,38,53], [128,51,108], [41,20,51], [25,16,3], [102,71,71], [77,54,71], [143,122,153], [42,41,51], [4,0,51], [31,54,77], [204,255,251], [51,128,77], [61,153,31], [194,204,143], [255,234,204], [204,119,0], [204,102,102],[64, 0, 0], [191, 0, 0], [64, 128, 0], [191, 128, 0],[64, 0, 128], [191, 0, 128], [64, 128, 128], [191, 128, 128],[0, 64, 0], [128, 64, 0], [0, 191, 0], [128, 191, 0],[0, 64, 128], [128, 64, 128]] # first 90 for the regular colors and the last 14 for objects having more than one segment
+        if is_overlay:
+            image1 = image_name.replace("png", "jpg")
+            video = '_'.join(image_name.split('_')[:2])
+            image1_overlay = Image.open(os.path.join(images_root_directory, video+'/'+image1))
 
-        a = overlay_semantic_mask(image1_val, small_lable, colors=davis_palette, alpha=0.2, contour_thickness=1)
-        img2 = Image.fromarray(a, 'RGB')
-        img2.save(output_directory + image_name)
+            if (image1_overlay.size != output_resolution):
+                image1_overlay = image1_overlay.resize(output_resolution)
+                
+            davis_palette = np.repeat(np.expand_dims(np.arange(0, 256), 1), 3, 1).astype(np.uint8)
+            davis_palette[:104, :] = [[0,0,0],[200, 0, 0], [0, 200, 0],[200, 128, 0], [0, 0, 200], [200, 0, 200], [0, 200, 200], [200, 200, 200],[252,93,82], [160,121,99], [164,188,119], [0,60,29], [75,237,255], [148,169,183], [96,74,207], [255,186,255], [255,218,231], [136,30,23], [231,181,131], [219,226,216], [0,196,107], [0,107,119], [0,125,227], [153,134,227], [91,0,56], [86,0,7], [246,207,195], [87,51,0], [125,131,122], [187,237,218], [46,57,59], [164,191,255], [37,29,57], [144,53,104], [79,53,54], [255,163,128], [255,233,180], [68,100,62], [0,231,199], [0,170,233], [0,20,103], [195,181,219], [148,122,135], [200,128,129], [46,20,10], [86,78,24], [180,255,188], [0,36,33], [0,101,139], [50,60,111], [188,81,205], [168,9,70], [167,91,59], [35,32,0], [0,124,28], [0,156,145], [0,36,57], [0,0,152], [89,12,97], [249,145,183],[255,153,170], [255,153,229], [184,143,204], [208,204,255], [11,0,128], [69,149,230], [82,204,194], [77,255,136], [6,26,0], [92,102,41], [102,85,61], [76,45,0], [229,69,69], [127,38,53], [128,51,108], [41,20,51], [25,16,3], [102,71,71], [77,54,71], [143,122,153], [42,41,51], [4,0,51], [31,54,77], [204,255,251], [51,128,77], [61,153,31], [194,204,143], [255,234,204], [204,119,0], [204,102,102],[64, 0, 0], [191, 0, 0], [64, 128, 0], [191, 128, 0],[64, 0, 128], [191, 0, 128], [64, 128, 128], [191, 128, 128],[0, 64, 0], [128, 64, 0], [0, 191, 0], [128, 191, 0],[0, 64, 128], [128, 64, 128]] # first 90 for the regular colors and the last 14 for objects having more than one segment
+    
+            a = overlay_semantic_mask(image1_overlay, out_image, colors=davis_palette, alpha=0.2, contour_thickness=1)
+            img2 = Image.fromarray(a, 'RGB')
+            img2.save(os.path.join(output_directory,image_name))
 
-    	'''
+    	
 
     
     #imwrite_indexed(output_directory + image_name, img,non_empty_objects)
@@ -117,19 +122,20 @@ def generate_masks_vos_edited(image_name, image_path, masks_info, output_directo
 
 
 
-def folder_of_jsons_to_masks_new(input_directory,output_directory,input_resolution=(1920,1080), output_resolution=(1920,1080)):
+def folder_of_jsons_to_masks_new(input_directory,output_directory,is_overlay=False, images_root_directory= '.',input_resolution=(1920,1080), output_resolution=(1920,1080)):
     import glob
     import csv
     import pandas as pd
     import os
+    from tqdm import tqdm
+
     
     if os.path.exists(os.path.join(output_directory,'data_mapping.csv')):
         os.remove(os.path.join(output_directory,'data_mapping.csv'))
         
-    for json_file in sorted(glob.glob(os.path.join(input_directory ,'*.json'))):
+    for json_file in tqdm(sorted(glob.glob(os.path.join(input_directory ,'*.json')))):
 
-        objects_keys = json_to_masks_new(json_file,output_directory,input_resolution=input_resolution, output_resolution=output_resolution)
-        print(objects_keys)
+        objects_keys = json_to_masks_new(json_file,output_directory,is_overlay=is_overlay, images_root_directory=images_root_directory,input_resolution=input_resolution, output_resolution=output_resolution)
         data = pd.DataFrame(objects_keys.items(), columns=['object_name', 'unique_index'])
         data['video_id'] = json_file.split('/')[-1].split('.')[0]
         
@@ -142,7 +148,7 @@ def folder_of_jsons_to_masks_new(input_directory,output_directory,input_resoluti
 
 
 
-def json_to_masks_new(filename,output_directory,object_keys=None,input_resolution=(1920,1080), output_resolution=(1920,1080)):
+def json_to_masks_new(filename,output_directory,is_overlay=False, images_root_directory= '.',input_resolution=(1920,1080), output_resolution=(1920,1080)):
     import os
     os.makedirs(output_directory, exist_ok=True)
     import json
@@ -155,7 +161,7 @@ def json_to_masks_new(filename,output_directory,object_keys=None,input_resolutio
         object_keys[key] = i
         i=i+1
     max_count = max(object_keys.values())
-    print(f'unique object count of {filename.split("/")[-1]} is {max_count}')
+    #print(f'unique object count of {filename.split("/")[-1]} is {max_count}')
     
     f = open(filename)
     # returns JSON object as a dictionary
@@ -173,7 +179,7 @@ def json_to_masks_new(filename,output_directory,object_keys=None,input_resolutio
         os.makedirs(full_path,exist_ok= True)
         #generate_masks_stage3(image_name, image_path, masks_info, full_path) #this is for saving the same name (delete the if statemnt as well)
         #generate_masks_per_seq(image_name, image_path, masks_info, full_path)
-        generate_masks_for_image(image_name, image_path, masks_info, full_path,object_keys=object_keys,input_resolution=input_resolution, output_resolution=output_resolution) #this is for unique id for each object throughout the video
+        generate_masks_for_image(image_name, image_path, masks_info, full_path,object_keys=object_keys,is_overlay=is_overlay,images_root_directory=images_root_directory,input_resolution=input_resolution, output_resolution=output_resolution) #this is for unique id for each object throughout the video
     return object_keys
 
 
@@ -240,6 +246,12 @@ def do_stats_stage2_jsons_single_file_new(file):
 
 
 
+def generate_video_from_images (input_directory,frame_rate):
+    import os
+
+    out_file = input_directory
+    x = "ffmpeg -y -pattern_type glob -r " + str(frame_rate)+ " -i '" +input_directory + "/*.png' -vcodec mpeg4 -qmin 1 -qscale:v 1 " + out_file + ".mp4"
+    os.system(x)
 
 def overlay_semantic_mask(im, ann, alpha=0.5, colors=None, contour_thickness=None):
     import numpy as np
